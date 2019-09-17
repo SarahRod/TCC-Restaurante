8adc03b07490f786f57e6e4c9f9fca2174179d32
Sarah Rodrigues <sarinha.santana02022000@gmail.com>
2019-09-17 10:11:40 -0300

    Fazendo o cadastro de produto

--- Restaurante/Restaurante/src/componentes/formulario/cadastro/produto/CadastroImagem.js
+++ Restaurante/Restaurante/src/componentes/formulario/cadastro/produto/CadastroImagem.js
@@ -77,27 +77,40 @@ export class CadastroImagem extends Component{
     render(){
         return(
             <Fragment>
-                <div class="col-12 col-md-7 red">
+                <div class="col-12 col-md-7">
+                    <h4>2º Passo</h4>
+                    <hr/>
                     <div class="row">
-                        <div class="col-7 col-md-5 mx-auto ">
+                        <div class="col-7 col-md-5">
                             <div class="card card-maior">
                                 <img src={Carne} class="card-img-top tamanho-imagem-produto" alt="..."/>
                                 <div class="card-body pb-0">
-                                    <h6 class="card-title text-center">Imagem Principal</h6> 
                                     <p>
-                                        <div class="input-group input-group-sm mb-0 ">
+                                        <div class="input-group input-group-sm ">
                                             <input type="text" class="form-control mt-0" aria-label="" id="legenda" name="legenda" placeholder="Escreva uma legenda.." value={this.state.Imagem.legenda}  onChange={e => this.atualizaCampo(e)}/>
                                             
                                         </div>
                                     </p>
-                                    <div className="input-file btn btn-light">
+                                    <div className="input-file btn btn-light mt-0 ml-4">
                                                 <span>Anexar Imagem</span>
                                                 <input ref="file" type="file" className="upload"  multiple="true" id="foto" name="foto" value={this.state.Imagem.foto}  onChange={e => this.atualizaCampo(e)}/>
                                             </div>
-                                    <a class="btn btn-success btn-sm mb-2 btn-tamanho" id="addInput" onClick={e => this.enviaImagem(e)} >
-                                        <span class="glyphicon glyphicon-plus" aria-hidden="true"></span>
-                                        Adicionar
-                                    </a>
+                                    <div className="row">
+                                        <div className="col-md-6 col-12" >
+                                            <a class="btn btn-success btn-sm mb-2 btn-tamanho" id="addInput" onClick={e => this.enviaImagem(e)} >
+                                                <span class="glyphicon glyphicon-plus" aria-hidden="true"></span>
+                                                Add
+                                            </a>
+                                        </div>
+                                        <div className="col-md-6 col-12" >
+                                            <a class="btn btn-success btn-sm mb-2 btn-tamanho" id="addInput" onClick={e => this.enviaImagem(e)} >
+                                                <span class="glyphicon glyphicon-plus" aria-hidden="true"></span>
+                                                Salvar
+                                            </a>    
+                                        </div>
+                                    </div>
+                                    
+                                    
                                 </div>
                             </div>
                         </div>   
--- Restaurante/Restaurante/src/componentes/formulario/cadastro/produto/CadastroProduto.js
+++ Restaurante/Restaurante/src/componentes/formulario/cadastro/produto/CadastroProduto.js
@@ -3,6 +3,7 @@ import { CadastroImagem } from './CadastroImagem';
 import $ from 'jquery';
 import { Link } from 'react-router-dom';
 import { DOMINIO } from '../../../../link_config';
+import { SessaoCategoria } from './SessaoCategoria';
 
 //ARMAZENA OS ESTADOS INICIAIS
 const initialState = {
@@ -16,7 +17,13 @@ const initialState = {
 
     restaurante: {
         id: 1
-    }
+    },
+
+    categoria: {
+        nome: ''
+    },
+
+    categoria:[]
    
 }
 
@@ -34,9 +41,6 @@ export class CadastroProduto extends Component {
         var novoproduto = { ...produto, 'restaurante': this.state.restaurante };
 
         const url = `${DOMINIO}/produto/novo`;
-
-        
-
 
         $.ajax({
 
@@ -77,60 +81,52 @@ export class CadastroProduto extends Component {
         return (
             <Fragment>
                 <div className="container">
-                    <div className="row mt-3">
+                    <div className="row mt-3 mb-5">
                         <h1 className="mx-auto">Cadastro de Produtos</h1>
                     </div>
-                    <hr />
                     <form>
                         <div className="row">
                             <div className="col-12 col-md-5">
-                                <div className="row mb-3">
+                                <h4>1º Passo</h4>
+                                <hr/>
+                                <div className="row mb-4">
                                     <div className="col-12">
                                         <label className="h5">Nome do Produto</label>
                                         <input className="form-control  mb-2 mr-sm-2" type="text" name="nome" id="nome" value={this.state.produto.nome} onChange={e => this.atualizaCampo(e)} />
                                     </div>
                                 </div>
                                 <div className="row mb-3">
-                                    <div className="col-3 mb-3">
+                                    <div className="col-3 mb-4">
                                         <label className="h5">Preço</label>
                                         <input className="form-control  mb-2 mr-sm-2" type="text" name="preco" id="preco" value={this.state.produto.preco} onChange={e => this.atualizaCampo(e)} />
                                     </div>
-                                    <div className="col-3 mb-3">
+                                    <div className="col-3 mb-4">
                                         <label className="h5">Promoção</label>
                                         <input className="form-control  mb-2 mr-sm-2" type="text" name="desconto" id="desconto" value={this.state.produto.desconto} onChange={e => this.atualizaCampo(e)} />
                                     </div>
-                                    <div className="col-6 mb-3">
+                                    <div className="col-6 mb-4">
                                         <h5 className="simbolo-porcentagem">%</h5>
                                     </div>
                                 </div>
-                                <div className="row mb-3">
-                                    <div className="col-8">
-                                        <label className="h5">Categoria do Produto</label>
-                                        <select className="custom-select">
-                                            <option value="" selected >Selecione a categoria para este produto... </option>
-                                            <option value="1">One</option>
-                                            <option value="2">Two</option>
-                                            <option value="3">Three</option>
-                                        </select>
-                                    </div>
-                                </div>
-                                <div className="row mb-3">
+                                
+                                <div className="row mb-4">
                                     <div className="col-10">
                                         <div className="form-group">
                                             <label for="exampleFormControlTextarea1" className="h5">Descrição do Produto</label>
-                                            <textarea className="form-control" id="exampleFormControlTextarea1" rows="3" name="descricao"   value={this.state.produto.descricao} onChange={e => this.atualizaCampo(e)}></textarea>
+                                            <textarea className="form-control" id="exampleFormControlTextarea1" rows="4" name="descricao"   value={this.state.produto.descricao} onChange={e => this.atualizaCampo(e)}></textarea>
                                         </div>
                                     </div>
+                                </div>
+                                <div className="row mt-5 ml-2">
+                                    <Link className="btn btn-success  btn-sm text-white  mt-4 col-5 col-lg-5" id="add-campo" onClick={e => this.enviaFormulario(e)} >
+                                        <span className="glyphicon glyphicon-plus" aria-hidden="true"></span>
+                                        Próximo Passo
+                                    </Link>
                                 </div>
                             </div>
                             <CadastroImagem />
                         </div>
-                        <div>
-                            <Link className="btn btn-success  btn-sm text-white  mt-4 col-5 col-lg-5" id="add-campo" onClick={e => this.enviaFormulario(e)} >
-                                <span className="glyphicon glyphicon-plus" aria-hidden="true"></span>
-                                Cadastrar
-                            </Link>
-                        </div>
+                        <SessaoCategoria/>
                     </form>
                 </div>
             </Fragment>
