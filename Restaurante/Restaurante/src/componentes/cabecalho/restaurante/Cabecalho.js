a30499f35ba42e7b0cc0d58f1cb1091657731826
MARINA SANTANA VAZ <18175241@CFP127ALUNO.LOCAL>
2019-09-17 14:31:34 -0300

    Arrumando cabecalho do restaurante e baixando o react boostrap

--- Restaurante/Restaurante/src/componentes/cabecalho/restaurante/Cabecalho.js
+++ Restaurante/Restaurante/src/componentes/cabecalho/restaurante/Cabecalho.js
@@ -9,6 +9,7 @@ import { DOMINIO } from '../../../link_config';
 import $ from 'jquery';
 import { BotaoLink } from '../../globais/botao/Botao';
 import { withRouter, Link } from 'react-router-dom';
+import { Navbar, Nav, Form, NavDropdown, FormControl, Button } from 'react-bootstrap';
 
 export class CabecalhoPaginaRestaurante extends Component {
 
@@ -56,31 +57,31 @@ export class CabecalhoPaginaRestaurante extends Component {
 
     render() {
         return (
-            <nav className="navbar navbar-expand-lg navbar-light bg-light mb-2">
+            <Navbar bg="light" expand="lg">
                 <Link className="navbar-brand" to="/restaurante">
                     <img src={Logo} style={{ maxWidth: 180 + 'px' }} />
                 </Link>
+                <Navbar.Toggle aria-controls="basic-navbar-nav" />
+                <Navbar.Collapse id="basic-navbar-nav">
+                    <OpcoesMenu className="row w-100 align-items-center ml-auto bg-light mr-4">
+                        <div className="nav-item col col-sm col-md col-lg ">
+                            <Link className="nav-link text-secondary" to="/restaurante">Pedidos</Link>
+                        </div>
+                        <div className="nav-item col col-sm col-md col-lg">
+                            <Link className="nav-link text-secondary" to="/restaurante/cadastro-produto">Cadastrar Produto</Link>
+                        </div>
+                        <div className="nav-item col col-sm col-md col-lg">
+                            <Link className="nav-link text-secondary" to="/restaurante/visualizar-produto">Catálogo de Produtos</Link>
+                        </div>
 
-                <OpcoesMenu className="row w-100 align-items-center ml-auto bg-light mr-4">
-                    <div className="nav-item col col-sm col-md col-lg ">
-                        <Link className="nav-link text-secondary" to="/restaurante">Pedidos</Link>
-                    </div>
-                    <div className="nav-item col col-sm col-md col-lg">
-                        <Link className="nav-link text-secondary" to="/restaurante/cadastro-produto">Cadastrar Produto</Link>
-                    </div>
-                    <div className="nav-item col col-sm col-md col-lg">
-                        <Link className="nav-link text-secondary" to="/restaurante/visualizar-produto">Catálogo de Produtos</Link>
-                    </div>
+                        <img className="border rounded-circle foto-restaurante  mr-4" src='' style={{ width: 65 + 'px', height: 60 + 'px' }} />
 
-                    <img className="border rounded-circle foto-restaurante  mr-4" src='' style={{ width: 65 + 'px', height: 60 + 'px' }} />
+                        <BotaoLink to="/" class="col col-sm col-md col-lg-3" texto="Logout" onClick={e => this.apagarLocalStorage(e)} />
 
-                    <BotaoLink to="/" class="col col-sm col-md col-lg-3" texto="Logout" onClick={e => this.apagarLocalStorage(e)} />
+                    </OpcoesMenu>
 
-                </OpcoesMenu>
-
-
-
-            </nav>
+                </Navbar.Collapse>
+            </Navbar>
         )
     }
 }
