35ca06f6369f10eb537cbac7aa56ea3adeb2df19
Marina Santana <marinasantana7894@gmail.com>
2019-10-03 19:51:12 -0300

    Menu já está ficando selecionado, filtros dos itens ativos e desativos riado(só falta as querys)

--- Restaurante/src/componentes/cabecalho/restaurante/Cabecalho.js
+++ Restaurante/src/componentes/cabecalho/restaurante/Cabecalho.js
@@ -7,6 +7,7 @@ import { BotaoLink } from '../../globais/botao/Botao';
 import { LinksMenu } from '../../globais/botao/styled';
 import { withRouter, Link } from 'react-router-dom';
 import { Navbar, Nav, Form, NavDropdown, FormControl, Button } from 'react-bootstrap';
+import './style.css';
 
 export class CabecalhoPaginaRestaurante extends Component {
 
@@ -17,6 +18,13 @@ export class CabecalhoPaginaRestaurante extends Component {
         sessionStorage.clear();
 
         this.props.history.push("/cadastro/endereco");
+    }
+
+    componentDidUpdate(){
+        $('a').click(function(){
+            $('a').removeClass('border-bottom-laranja');
+            $(this).addClass('border-bottom-laranja');
+        });
     }
 
     componentDidMount() {
@@ -57,13 +65,13 @@ export class CabecalhoPaginaRestaurante extends Component {
                 <Navbar.Collapse id="basic-navbar-nav">
                     <OpcoesMenu className="row w-100 align-items-center ml-auto bg-light mr-4">
                         <div className="nav-item col col-sm col-md col-lg ">
-                            <LinksMenu className="nav-link text-secondary" to="/restaurante">Pedidos</LinksMenu>
+                            <LinksMenu className="nav-link text-secondary text-center" to="/restaurante">Pedidos</LinksMenu>
                         </div>
                         <div className="nav-item col col-sm col-md col-lg">
-                            <Link className="nav-link text-secondary" to="/restaurante/cadastro-produto">Cadastrar Produto</Link>
+                            <Link className="nav-link text-secondary text-center" to="/restaurante/cadastro-produto">Cadastrar Produto</Link>
                         </div>
                         <div className="nav-item col col-sm col-md col-lg">
-                            <Link className="nav-link text-secondary" to="/restaurante/visualizar-produto">Catálogo de Produtos</Link>
+                            <Link className="nav-link text-secondary text-center" to="/restaurante/visualizar-produto">Catálogo de Produtos</Link>
                         </div>
 
                         <img className="border rounded-circle foto-restaurante  mr-4" src='' style={{ width: 65 + 'px', height: 60 + 'px' }} />
