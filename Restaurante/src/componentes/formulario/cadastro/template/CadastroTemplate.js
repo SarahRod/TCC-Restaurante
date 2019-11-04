3d74745aa83a7fe2c0a21fbfa05e0e995f73b815
SARAH RODRIGUES SANTANA <18175198@CFP127ALUNO.LOCAL>
2019-11-04 16:07:52 -0300

    Cadastro de Template quase pronto, só falta colocar na pagina index.html

--- Restaurante/src/componentes/formulario/cadastro/template/CadastroTemplate.js
+++ Restaurante/src/componentes/formulario/cadastro/template/CadastroTemplate.js
@@ -1,7 +1,10 @@
 import React, {Component, Fragment} from 'react';
 import {Link} from 'react-router-dom';
 import { DOMINIO, TOKEN, DOMINIO_IMG, TEMPLATE_IMG_1, TEMPLATE_IMG_2, TEMPLATE_IMG_3 } from '../../../../link_config';
+import { ToastContainer} from 'react-toastify';
+import { Notificacao,ERRO, CAMPO_VAZIO,  } from '../../../../funcoes/Alerta/index';
 import $ from 'jquery';
+import { Alert } from 'react-bootstrap';
 
 
 export class CadastroTemplate extends Component{
@@ -11,7 +14,7 @@ export class CadastroTemplate extends Component{
         this.state={
             
             template:{
-                foto: '',
+                foto: `${TEMPLATE_IMG_1}`,
                 sobre: '',
                 slogan: '',
                 
@@ -23,15 +26,38 @@ export class CadastroTemplate extends Component{
         }
     }
 
+    // getInitialState = {
+    //       selectedOption: 'option1'
+    //   }
+
 
 
     atualizaCampo(e){
         const template = {...this.state.template};
-        template[e.target.name] = e.target.value
+        template[e.target.name]= e.target.value
+        
+        
+        
         
         this.setState({
-            template
+           template
+            
         })
+        // console.log(template);
+
+    }
+
+
+    validaCampos(e){
+        e.preventDefault();
+
+        if (!$('#descricao-principal').val() || !$('#sobre-nos').val() ) {
+            Notificacao(ERRO,CAMPO_VAZIO);
+           
+        } else {
+            this.cadastroTemplate(e);
+            
+        }
 
     }
     
@@ -50,7 +76,8 @@ export class CadastroTemplate extends Component{
 
             success: function (resposta) {
 
-              console.log(resposta)
+              console.log(resposta);
+              this.props.history.push("/restaurante");
 
             }.bind(this),
             error: function (data) {
@@ -66,56 +93,65 @@ export class CadastroTemplate extends Component{
     render(){
         return(
             <Fragment>
-                <div class=" container">
-                    <h1 class="mb-3 text-center mb-5">Sua página</h1>
-                    <h5>Conte sobre sua empresa</h5>
-                    <hr/>
-                    <div class="row mt-4">
-                        <div class="col-md-6 col-12">
-                            <label class="h6">Descrição Principal</label><br/>
-                            <textarea class="form-control" style={{height: 150 +'px'}} id="descricao-principal" value={this.state.template.slogan} name="slogan"  onChange={e => this.atualizaCampo(e)}></textarea>
-                        </div>
-                        <div class="col-md-6 col-12">
-                            <label class="h6">Sobre nós</label><br/>
-                            <textarea class="form-control"  style={{height: 150 +'px'}} id="sobre-nos" name="sobre" value={this.state.template.sobre} onChange={e => this.atualizaCampo(e)}></textarea>
-                        </div>
-                    </div>
-                    
-                    <h5 class="mt-5">Plano de fundo</h5>
-                    <hr/>
-                    <div class="row mt-4 mb-5">
-                        <div class="col-12 col-md-4">
-                            <div class="card mb-3">
-                                <img class="card-img-top" style={{height: 220 + 'px'}} src={`${DOMINIO_IMG}`+ `${TEMPLATE_IMG_1}`} alt="imagem Template 01"/>
+                <form>
+                    <div class=" container">
+                        <h1 class="mb-3 text-center mb-5">Sua página</h1>
+                        <h5>Conte sobre sua empresa</h5>
+                        <hr/>
+                        <div class="row mt-4">
+                            <div class="col-md-6 col-12">
+                                <label class="h6">Descrição Principal</label><br/>
+                                <textarea class="form-control" style={{height: 150 +'px'}} id="descricao-principal" value={this.state.template.slogan} name="slogan"  onChange={e => this.atualizaCampo(e)}></textarea>
                             </div>
-                            <input class="mr-3" type="radio" name="imagem" style={{boxShadow: 'none'}} value={`${DOMINIO_IMG}`+ `${TEMPLATE_IMG_1}`}  onChange={e => this.atualizaCampo(e)}/>Imagem 01
-                        </div>
-                        <div class="col-12 col-md-4">
-                            <div class="card mb-3">
-                                <img class="card-img-top" style={{height: 220 + 'px'}} src={`${DOMINIO_IMG}`+ `${TEMPLATE_IMG_2}`} alt="imagem Template 02"/>
+                            <div class="col-md-6 col-12">
+                                <label class="h6">Sobre nós</label><br/>
+                                <textarea class="form-control"  style={{height: 150 +'px'}} id="sobre-nos" name="sobre" value={this.state.template.sobre} onChange={e => this.atualizaCampo(e)}></textarea>
                             </div>
-                            <input class="mr-3" type="radio" name="imagem" style={{boxShadow: 'none'}} value={`${DOMINIO_IMG}`+ `${TEMPLATE_IMG_2}`}  onChange={e => this.atualizaCampo(e)}/>Imagem 02
                         </div>
-                        <div class="col-12 col-md-4">
-                            <div class="card mb-3">
-                                <img class="card-img-top" style={{height: 220 + 'px'}} src={`${DOMINIO_IMG}`+ `${TEMPLATE_IMG_3}`} alt="imagem Template 03"/>
+                        
+                        <h5 class="mt-5">Plano de fundo</h5>
+                        <hr/>
+                        <div class="row mt-4 mb-5">
+                            <div class="col-12 col-md-4">
+                                <div class="card mb-3">
+                                    <img class="card-img-top" style={{height: 220 + 'px'}} src={`${DOMINIO_IMG}`+ `${TEMPLATE_IMG_1}`} alt="imagem Template 01"/>
+                                </div>
+                                <input class="mr-3" type="radio" name="foto" defaultChecked     style={{boxShadow: 'none'}}
+                                                                checked={this.state.foto} 
+                                                                value={`${TEMPLATE_IMG_1}`} 
+                                                                onChange={e => this.atualizaCampo(e)}/>Imagem 01
                             </div>
-                            
-                            <input class="mr-3" type="radio" style={{boxShadow: 'none'}} name="imagem" value={`${DOMINIO_IMG}`+ `${TEMPLATE_IMG_3}`} onChange={e => this.atualizaCampo(e)}/>Imagem 03
+                            <div class="col-12 col-md-4">
+                                <div class="card mb-3">
+                                    <img class="card-img-top" style={{height: 220 + 'px'}} src={`${DOMINIO_IMG}`+ `${TEMPLATE_IMG_2}`} alt="imagem Template 02"/>
+                                </div>
+                                <input class="mr-3" type="radio" name="foto" style={{boxShadow: 'none'}} 
+                                                                checked={this.state.foto}
+                                                                value={`${TEMPLATE_IMG_2}`} 
+                                                                onChange={e => this.atualizaCampo(e)}/>Imagem 02
+                            </div>
+                            <div class="col-12 col-md-4">
+                                <div class="card mb-3">
+                                    <img class="card-img-top" style={{height: 220 + 'px'}} src={`${DOMINIO_IMG}`+ `${TEMPLATE_IMG_3}`} alt="imagem Template 03"/>
+                                </div>
+                                
+                                <input class="mr-3" type="radio" style={{boxShadow: 'none'}} name="foto" 
+                                                            checked={this.state.foto}
+                                                            value={`${TEMPLATE_IMG_3}`} 
+                                                            onChange={e => this.atualizaCampo(e)}/>Imagem 03
+                            </div>
                         </div>
-                    </div>
-                    <div className="row">
-                    <div className="col-2 ml-auto">
-                        <div className="col col-sm col-md   col-lg">
-                            <Link class="btn btn-success" onClick={e => this.cadastroTemplate(e)}>Finalizar</Link>
-                            {/* onclik={this.cadastroTemplate()} */}
-                            {/* to="/restaurante" */}
+                        <div className="row">
+                        <div className="col-2 ml-auto mb-5">
+                            <div className="col col-sm col-md   col-lg">
+                                <Link class="btn btn-success" onClick={e => this.validaCampos(e)}>Finalizar</Link>
+                            </div>
                         </div>
+                        </div>
+                        
+                        <ToastContainer/>
                     </div>
-                    </div>
-                    
-                
-                </div>
+                </form>
             </Fragment>
         )
     }
